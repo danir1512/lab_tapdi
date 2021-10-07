f668a283c149e474632480929e013476b6fd1c95
jpal-oliveira <63066324+jpal-oliveira@users.noreply.github.com>
2021-10-07 13:00:43 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 import cv2 as cv
 import tkinter as tk
-import tkinter.filedialog as window
-import imageForms as forms
-#root = tk.Tk()
-#root.withdraw()
-#file_path = window.askopenfilename()
+import tkinter.filedialog as fd
+import imageForms as IF
 
-# abrir imagem
-pathname = "/Users/d.genovse/Desktop/TAPDI/"
-filename = "peppers.jpeg"
-img = cv.imread(pathname + filename)
+#abrir imagem
+root = tk.Tk()
+root.withdraw()
+file_path = fd.askopenfilename()
 
-#if file_path != "":
-#img = cv.imread(file_path)
+img = cv.imread(file_path)
+newImg = img
 imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-ret, imgBW = cv.threshold(imgGray, 12, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-#forms.showSideBySideImages(imgGray,imgBW,"teste")
-imgBlur = cv.blur(img,(11,11))
-dimensions = img.shape
+ret,imgBW = cv.threshold(imgGray, 12, 255, cv.THRESH_BINARY, cv.THRESH_OTSU)
+IF.showSideBySideImages(img, imgGray, "Imagens", False, False)
+imgBlur = cv.blur(img, (3,3))
 
-'''
-isto é um teste
-'''
+height = img.shape[0]
+width = img.shape[1]
+channels = img.shape[2]
 
+brightness = input("Brightness: ")
+contrast = input("Contrast: ")
 
-print("o joao é feio")
+for x in range(height):
+    for y in range(width):
+        for z in range(3):
+            newBit = int(img[x, y, z] * contrast + brightness)
+            if newBit > 255:
+                newBit = 255
+            elif newBit < 0:
+                newBit = 0
+            else:
+                newImg[x,y,z] = newBit
 
-###vamos ver
-cv.imshow("Imagem", imgBlur)
-#cv.imshow("Imagem", imgBW)
+cv.imshow("Imagem", newImg)
 cv.waitKey()
-
-
-
